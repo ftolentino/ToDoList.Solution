6458a888b622b5d1da5ba2d89894514ba97662b4
ftolentino <filmer.tolentino@gmail.com>
2022-07-20 21:32:26 -0700

    Objects within Objects lesson

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -33,9 +33,11 @@ namespace ToDoList
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
 
+      app.UseStaticFiles();
+
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Hello World! Something's not connected properly");
       });
     }
   }
